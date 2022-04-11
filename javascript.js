472ede370e29cf68f903fbe651d2eb168007cc2f
Andreas Hochmuth <hochmuth.andreas@gmail.com>
2022-04-11 17:09:27 -0400

    Added GUI to RPS game with output on webpage

--- javascript.js
+++ javascript.js
@@ -1,103 +1,146 @@
 
+let computerScore = 0
+let humanScore = 0
 
 let computerPlay = () => {
-    let randomNum = Math.floor(Math.random()*3)
+    let randomNum = Math.floor(Math.random() * 3);
     if (randomNum === 0) {
         return "rock";
     }
-        
-        else if (randomNum === 1) {
-            return "paper"
-        }
-        else {
-            return "scissors"
-        }
-    
+
+    else if (randomNum === 1) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+
 }
 
-let playRound = (humanChoice,computerChoice) => {
-    humanChoice = humanChoice.toLowerCase()
-if (humanChoice === computerChoice) {
-    return "The game is a tie!";}
+let playRound = (humanChoice, computerChoice) => {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+        return "The game is a tie!";
+    }
 
     else if (humanChoice === "rock") {
         if (computerChoice === "paper") {
-            return "computer";}
+            return "computer";
+        }
 
-            else if (computerChoice === "scissors") {
-                return "human"
+        else if (computerChoice === "scissors") {
+            return "human";
         }
         else {
-
         }
-    
+
 
 
     }
     else if (humanChoice === "paper") {
-    if (computerChoice === "scissors") {
-        return "computer";}
+        if (computerChoice === "scissors") {
+            return "computer";
+        }
 
         else if (computerChoice === "rock") {
-            return "human"
+            return "human";
         }
         else {
-
         }
-    
+
 
 
     }
     else if (humanChoice === "scissors") {
-    if (computerChoice === "rock") {
-        return "computer";}
+        if (computerChoice === "rock") {
+            return "computer";
+        }
 
         else if (computerChoice === "paper") {
-            return "human"
+            return "human";
         }
-       else {
-       }
+        else {
+        }
+
+
+
+    }
+}
+
+
+const btn1 = document.querySelector('#btn1');
+btn1.addEventListener('click', () => {
+  humanChoice = 'rock'
+  game();
+});
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+  humanChoice = 'paper'
+  game();
+});
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', () => {
+  humanChoice = 'scissors'
+  game();
+});
+
     
 
 
-}}
-
-
-
 let game = () => {
-    let computerScore = 0
-    let humanScore = 0
-    for (let i = 0; i<5; i++){
-        let humanChoice = prompt("What would you like to play? Rock, paper, or scissors?")
+    
+    
+        
+        
         let computerChoice = computerPlay()
         let result = playRound(humanChoice,computerChoice)
         
         if (result === "computer"){
             computerScore = computerScore + 1;
-            console.log('The Computer Won');
+            document.getElementById("scores").innerHTML = ('<p>Your score is: ' + humanScore + ' The computer score is: ' + computerScore + '</p>')
+            document.getElementById("outputs").innerHTML = ('The computer won! The computer chose ' + computerChoice + ' and you chose ' + humanChoice)
+            
         }
         
             else if (result === "human"){
             humanScore = humanScore + 1;
-            console.log('You Won!')
+            document.getElementById("scores").innerHTML = ('<p>Your score is: ' + humanScore + ' The computer score is: ' + computerScore + '</p>')
+            document.getElementById("outputs").innerHTML = ('You won! The computer chose ' + computerChoice + ' and you chose ' + humanChoice)
+            
         }
 
                 else {
-                console.log('It was a tie!')}
-
-
-    
-    console.log("Human Choice: ",humanChoice);
-    console.log("Computer Choice: ",computerChoice);}
-
+                document.getElementById("scores").innerHTML = ('<p>Your score is: ' + humanScore + ' The computer score is: ' + computerScore + '</p>')
+                document.getElementById("outputs").innerHTML = ('It was a tie! The computer chose ' + computerChoice + ' and you chose ' + humanChoice)
+               
 
 
 
     
-console.log("You scored:",humanScore,"The computer scored: ",computerScore)
+    
+
+
+
+
+
+    
+
+
 
 }
 
-
-game();
-
+if (computerScore < 5 && humanScore < 5) {
+}
+else if (computerScore > 4)  {
+    computerScore = 0
+    humanScore = 0  
+    document.getElementById("scores").innerHTML = ('<p>The game has finished and reset with the computer reaching 5 first! The human score is now ' + humanScore + ' And computer score ' + computerScore + '</p>')
+    document.getElementById("outputs").innerHTML = ('The computer won! The computer chose ' + computerChoice + ' and you chose ' + humanChoice)
+}
+else {
+    computerScore = 0
+    humanScore = 0  
+    document.getElementById("scores").innerHTML = ('<p>The game has finished and reset with you reaching 5 first! The human score is now ' + humanScore + ' And computer score ' + computerScore + '</p>')
+    document.getElementById("outputs").innerHTML = ('You won! The computer chose ' + computerChoice + ' and you chose ' + humanChoice)
+}
+}
